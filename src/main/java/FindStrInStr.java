c70c01ee5e50d95531fddd4e392ef2b69bb77067
Rathod009 <52994515+Rathod009@users.noreply.github.com>
2020-10-01 08:52:39 +0530

    Update FindStrInStr.java
    Faster

--- src/main/java/FindStrInStr.java
+++ src/main/java/FindStrInStr.java
@@ -42,3 +42,69 @@ public class FindStrInStr {
         return -1;
     }
 }
+
+/** time complaxity of this logic is less compare to above! 
+
+    public int strStr(String haystack, String needle) {
+        
+        if(needle.length()==0 || needle == null ) return 0;
+        if(needle.length()>haystack.length()) return -1;
+        
+        char[] hs = new char[haystack.length()];
+        char[] nd = new char[needle.length()];
+        hs = haystack.toCharArray();
+        nd = needle.toCharArray();
+        
+        int mainIndex=0;
+        int i=1;
+        
+        int[] indexData = new int[nd.length];
+        indexData[0] = 0;
+        
+        while(i<nd.length){
+        	
+            if(nd[i] == nd[mainIndex]){
+            	mainIndex++;
+                indexData[i] = mainIndex;
+                i++;
+                
+            }
+            else{
+                if(mainIndex!=0){
+                	mainIndex = indexData[mainIndex-1];
+                }
+                else{
+                    indexData[i]=0;
+                    i++;
+                }
+            }
+            
+        }
+        
+        int cmpr=0;
+        
+        for(i=0;i<hs.length;i++){
+            
+            if(hs[i] == nd[cmpr]){
+                cmpr++;
+                if(cmpr == nd.length) return i - cmpr +1;
+            }
+            else if(hs[i] != nd[cmpr] && cmpr!=0){
+                cmpr = indexData[cmpr-1];
+                i--;
+            }
+        }
+        return -1;
+    }
+    
+ 
+    
+  
+    
+      
+    
+    
+    
+    
+
+*/
